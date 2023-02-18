3fa551df5208584d9e3fe088c44baee96efe9ca7
BillionOla <billionolagbaye5@gmail.com>
2023-02-18 17:27:34 -0500

    Icon and weather updated

--- src/index.js
+++ src/index.js
@@ -15,25 +15,34 @@ let day = days[now.getDay()];
 let date = [now.getDate()];
 let hours = [now.getHours()];
 let minutes = [now.getMinutes()];
+//let months = {now.getMonths()};
 
 p.innerHTML = `${day}, ${date}. ${hours}:${minutes}`;
 
 function showResponse(response) {
-  let cityName = document.querySelector("#city");
-  let temperature = document.querySelector("#temp");
-  let humidity = document.querySelector("#hum");
-  let wind = document.querySelector("#windval");
-  let temperatureval = Math.round(response.data.main.temp);
-  let humidityval = Math.round(response.data.main.humidity);
-  let windval = Math.round(response.data.wind.speed);
-  let cityval = response.data.name;
-  temperature.innerHTML = temperatureval;
-  humidity.innerHTML = humidityval;
-  wind.innerHTML = windval;
-  cityName.innerHTML = cityval;
+  let cityVal = document.querySelector("#city");
+  let temperatureVal = document.querySelector("#temp");
+  let humidityVal = document.querySelector("#hum");
+  let windVal = document.querySelector("#windval");
+  let iconVal = document.querySelector("#icon")
+ let descriptionVal = document.querySelector("#descript");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureVal.innerHTML = Math.round(response.data.main.temp);
+  humidityVal.innerHTML = Math.round(response.data.main.humidity);
+  windVal.innerHTML = Math.round(response.data.wind.speed);
+  descriptionVal.innerHTML = response.data.weather[0].main;
+  cityVal.innerHTML = response.data.name;
+
+  iconVal.setAttribute(
+   "src",
+  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+   );
+ iconVal.setAttribute("alt", response.data.weather[0].description);
+
 }
 
-//City Input
 function searchCity(cityIn) {
   let apiKey = "2da40a2dd6ecb600a6befee8dc71a523";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityIn}&appid=${apiKey}&units=metric`;
@@ -45,27 +54,22 @@ function submitSearch(event) {
   let cityIn = document.querySelector("#city-input").value;
   searchCity(cityIn);
 }
-
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", submitSearch);
 
+//function searchLocation(coordinates) {
+  //let apiKey = "2da40a2dd6ecb600a6befee8dc71a523";
+  //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lon=${lon}&lat=${lat}&appid=${apiKey}&units=metric`;
+  //axios.get(apiUrl).then(showResponse);
+//}
+
+//function getCurrentPosition(event) {
+  //event.preventDefault();
+ // navigator.geolocation.getCurrentPosition(searchLocation);
+//}
+
+
+
+//let searchSubmit = document.querySelector("#search-form");
+//searchSubmit.addEventListener("submit", searchCity);
 searchCity("Avon");
-
-function searchLocation(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiKey = "2da40a2dd6ecb600a6befee8dc71a523";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lon=${lon}&lat=${lat}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showResponse);
-}
-
-function getCurrentPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-let currentLocation = document.querySelector("#current-button");
-currentLocation.addEventListener("click", getCurrentPosition);
-
-let searchSubmit = document.querySelector("#search-form");
-searchSubmit.addEventListener("submit", searchCity);
