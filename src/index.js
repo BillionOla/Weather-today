6147220e800c4770f92f92cd4c0ac1995ec04fb1
BillionOla <billionolagbaye5@gmail.com>
2023-02-18 20:21:18 -0500

    added a link and Celsius and fahrenheit.
    I want to the Fahrenheit to be clickable and display temperature in Fahrenheit

--- src/index.js
+++ src/index.js
@@ -40,15 +40,12 @@ function showResponse(response) {
   `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
    );
  iconVal.setAttribute("alt", response.data.weather[0].description);
-
 }
-
 function searchCity(cityIn) {
   let apiKey = "2da40a2dd6ecb600a6befee8dc71a523";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityIn}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showResponse);
 }
-
 function submitSearch(event) {
   event.preventDefault();
   let cityIn = document.querySelector("#city-input").value;
@@ -57,19 +54,20 @@ function submitSearch(event) {
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", submitSearch);
 
-//function searchLocation(coordinates) {
-  //let apiKey = "2da40a2dd6ecb600a6befee8dc71a523";
-  //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lon=${lon}&lat=${lat}&appid=${apiKey}&units=metric`;
-  //axios.get(apiUrl).then(showResponse);
-//}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
-//function getCurrentPosition(event) {
-  //event.preventDefault();
- // navigator.geolocation.getCurrentPosition(searchLocation);
-//}
+//
+function displayFahrenheitTemperature (event){
+  event.preventDefault();
+   let temperatureVal = document.querySelector("#temp");
+ let fahrenheitTemperature = (celsiusTemperature + 9) /5 + 32;
+ temperatureVal.innerHTML = Math.round(fahrenheitTemperature);
+}
+let celsiusTemperature=null; 
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 
-
-//let searchSubmit = document.querySelector("#search-form");
-//searchSubmit.addEventListener("submit", searchCity);
-searchCity("Avon");
+searchCity("New York");
